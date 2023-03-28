687931d6d294540d993554e80a898f23340e7291
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-28 00:21:47 -0300

    StreamLit with MySQL - getting data from db to streamLit chart

--- streamLit/streamLitApp.py
+++ streamLit/streamLitApp.py
@@ -1,10 +1,26 @@
 import streamlit as st
+import mysql.connector
 import pandas as pd
+import numpy as np
 
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="dev",
+  password="dev",
+  database="node_test"
+)
+tableName='node'
+#para pegar todos os dados da table do db
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM " + tableName)
+myresult = mycursor.fetchall()
+for result in myresult:
+  print(result)
 
+#adicionando dados para os linechart
 st.header('Compressor UTFPR - Informações Elétricas')
-
-if st.button('Corrente'):
-    st.write('DADOS DE CORRENTE AQUI')
-else:
-    st.write('Qual dado você gostaria de saber')
+df = pd.DataFrame(myresult, columns=['value','created_at'])
+datas = df[['value', 'created_at']]
+st.line_chart(datas)
+st.dataframe(df)
+st.line_chart(df[['value','created_at']], x = 'created_at',y='value')

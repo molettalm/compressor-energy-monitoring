4e5f4b9337c944679ec42265152464423223b9a0
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-27 23:44:26 -0300

    StreamLit first test - nothing that make senses, just a test

---
+++ streamLit/streamLitApp.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.header('Compressor UTFPR - Informações Elétricas')
+
+if st.button('Corrente'):
+    st.write('DADOS DE CORRENTE AQUI')
+else:
+    st.write('Qual dado você gostaria de saber')

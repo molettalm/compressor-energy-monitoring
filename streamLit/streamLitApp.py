bfe370e9ece730e257e5dbd7532017b9bf7080f8
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-28 18:57:34 -0300

    Starting data implementing for day charts

--- streamLit/streamLitApp.py
+++ streamLit/streamLitApp.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 mydb = mysql.connector.connect(
-  host="127.0.0.1",
+  host="localhost",
   user="dev",
   password="dev",
   database="node_test"
@@ -12,15 +12,17 @@ mydb = mysql.connector.connect(
 tableName='node'
 #para pegar todos os dados da table do db
 mycursor = mydb.cursor()
+
 mycursor.execute("SELECT * FROM " + tableName)
-myresult = mycursor.fetchall()
-for result in myresult:
-  print(result)
+dadosDB = mycursor.fetchall()
+for result in dadosDB:
+  print(result[1])
+  dataMedidas = result[1] #armazenando as datas do sql
 
 #adicionando dados para os linechart
 st.header('Compressor UTFPR - Informações Elétricas')
-df = pd.DataFrame(myresult, columns=['value','created_at'])
-datas = df[['value', 'created_at']]
-st.line_chart(datas)
-st.dataframe(df)
+d = st.date_input("Escolha o dia das medições: ") #Pegamos o dia que o usuário quer ver as medições
+#Fazer um dataframe com o panda separando pelo dia
+
+df = pd.DataFrame(dadosDB, columns=['value','created_at'])
 st.line_chart(df[['value','created_at']], x = 'created_at',y='value')

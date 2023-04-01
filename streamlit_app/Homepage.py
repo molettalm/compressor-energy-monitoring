97d5c1c974b04fe374855928495e32b10dac0777
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-04-01 12:17:20 -0300

    Error in metrics when out of data (no energy)

--- streamlit_app/Homepage.py
+++ streamlit_app/Homepage.py
@@ -60,82 +60,50 @@ def classifyOpMode(valor):
     else:
         return 'StandBy'
 
-numGetOld = round(df['current'].size/5)
-if(numGetOld < 1):
-    numGetOld = 1
-currentAvgOld = df['current'].iloc[:-numGetOld].mean()
-powerFacAvgOld = df['power_factor_calc'].iloc[:-numGetOld].mean()
-opModeOld = df['current'].iloc[:-numGetOld].apply(classifyOpMode)
-contOpModeOld = opModeOld.value_counts()
-#garantindo que a chave irá existir
-d = contOpModeOld.to_dict()
-d.setdefault('On', 0)
-d.setdefault('Off', 0)
-d.setdefault('Standby', 0)
-auxOld = pd.Series(d)
-#Observe que ao usar o método .setdefault(), as chaves só são criadas se não existirem no dicionário. Se a chave já existir, o método não faz nada.
-qtyOnOld = auxOld['On']
-qtyStandByOld = auxOld['StandBy']
-qtyOffOld = auxOld['Off']
-
-
-
 with st.container():
-    # st.write("Tensão")
-    # st.line_chart(df.rename(columns = {'voltage': 'Tensão (V)'})['Tensão (V)'], use_container_width=True) #Tensão
     fig = px.line(df, x = 'moment', y = 'voltage', title='Tensão [V]', labels = {'voltage': 'Tensão', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    # st.write("Corrente")
-    # st.line_chart(df.rename(columns = {'current': 'Corrente (A)'})['Corrente (A)'], use_container_width=True) #Corrente 
     fig = px.line(df, x = 'moment', y = 'current', title='Corrente [A]', labels = {'current': 'Corrente', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    # st.write("Potência")
-    # st.line_chart(df.rename(columns = {'power_W': 'Potência (W)'})['Potência (W)'], use_container_width=True) #Potencia
     fig = px.line(df, x = 'moment', y = 'power_W', title='Potência [W]', labels = {'power_W': 'Potência', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    # st.write("Energia")
-    # st.line_chart(df.rename(columns = {'energy_WH': 'Energia (W/h)'})['Energia (W/h)'], use_container_width=True) #Energia
     fig = px.line(df, x = 'moment', y = 'energy_WH', title='Energia [W/h]', labels = {'energy_WH': 'Energia', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    # st.write("Fator de Potência")
-    # st.line_chart(df.rename(columns = {'power_factor_measured': 'Fat de Pot Medido', 'power_factor_calc': 'Fat de Pot Calc'})[['Fat de Pot Medido', 'Fat de Pot Calc']], use_container_width=True) #Fator de Potência
     fig = px.line(df, x = 'moment', y = ['power_factor_measured', 'power_factor_calc'], title='Fator de Potência', color_discrete_map = {'power_factor_measured': 'green', 'power_factor_calc': 'blue'} , labels = {'power_factor_measured': 'Medido', 'power_factor_calc': 'Calculado', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    # st.write("Ângulo de Fase")
-    # st.line_chart(df.rename(columns = {'phase_angle_measured': 'Âng de Fase Medido (Graus)', 'phase_angle_calc': 'Âng de Fase Calc (Graus)'})[['Âng de Fase Medido (Graus)', 'Âng de Fase Calc (Graus)']], use_container_width=True) #Ângulo de Fase
     fig = px.line(df, x = 'moment', y = ['phase_angle_measured', 'phase_angle_calc'], title='Ângulo de Fase [Graus]', color_discrete_map = {'phase_angle_measured': 'green', 'phase_angle_calc': 'blue'} , labels = {'phase_angle_measured': 'Medido', 'phase_angle_calc': 'Calculado', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
     st.write("Modos de operação")
     chart_data = pd.DataFrame(np.random.randn(20,3), columns=["a", "b", "c"]) #Chart com a qtd de vezes do modo de operação
     st.bar_chart(chart_data) #Chart com a qtd de vezes do modo de operação
-#st.dataframe(df.reset_index()) 
 
 #Para as métricas de modo de operação, quantas vezes ficou ligado, quantas vezes desligado, o outro modo de operação lá e por fim a qtd de Ah do período
 with st.sidebar:
-    currentAvg = df['current'].mean()
-    powerFacAvg = df['power_factor_calc'].mean()
-    opMode = df['current'].apply(classifyOpMode)
-    contOpMode = opMode.value_counts()
-    #garantindo que a chave irá existir
-    d = contOpMode.to_dict()
-    d.setdefault('On', 0)
-    d.setdefault('Off', 0)
-    d.setdefault('Standby', 0)
-    aux = pd.Series(d)
-    #Observe que ao usar o método .setdefault(), as chaves só são criadas se não existirem no dicionário. Se a chave já existir, o método não faz nada.
-    qtyOn = aux['On']
-    qtyStandBy = aux['StandBy']
-    qtyOff = aux['Off']
-    st.metric("Corrente Média no período",  str(round(currentAvg, 3)) + " A", str(round(currentAvgOld-currentAvg, 3)) + " A" )
-    st.metric("Fator de Potência calculado no período", round(powerFacAvg, 3), round(powerFacAvgOld-powerFacAvg, 3))
-    st.metric("Quantidade de vezes ligado durante o período", str(qtyOn) + ' vezes', str(qtyOnOld-qtyOn) + ' vezes')
-    st.metric("Quantidade de vezes em standby durante o período", str(qtyStandBy) + ' vezes', str(qtyStandByOld-qtyStandBy) + ' vezes')
-    st.metric("Quantidade de vezes desligado durante o período", str(qtyOff) + ' vezes', str(qtyOffOld-qtyOff) + ' vezes')
+    if (len(df['current']) != 0):
+        currentAvg = df['current'].mean()
+        powerFacAvg = df['power_factor_calc'].mean()
+        opMode = df['current'].apply(classifyOpMode)
+        contOpMode = opMode.value_counts()
+        #garantindo que a chave irá existir
+        d = contOpMode.to_dict()
+        d.setdefault('On', 0)
+        d.setdefault('Off', 0)
+        d.setdefault('Standby', 0)
+        aux = pd.Series(d)
+        #Observe que ao usar o método .setdefault(), as chaves só são criadas se não existirem no dicionário. Se a chave já existir, o método não faz nada.
+        qtyOn = aux['On']
+        qtyStandBy = aux['StandBy']
+        qtyOff = aux['Off']
+        st.metric("Corrente Média no período",  str(round(currentAvg, 3)) + " A")
+        st.metric("Fator de Potência calculado no período", round(powerFacAvg, 3))
+        st.metric("Quantidade de vezes ligado durante o período", str(qtyOn) + ' vezes')
+        st.metric("Quantidade de vezes em standby durante o período", str(qtyStandBy) + ' vezes')
+        st.metric("Quantidade de vezes desligado durante o período", str(qtyOff) + ' vezes')
 
 st.text('Lucas Moletta')

ce1dbd10a1b504678deb49741f9d3c32a1ea35c0
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-04-01 11:17:01 -0300

    Metrics updated

--- streamlit_app/Homepage.py
+++ streamlit_app/Homepage.py
@@ -52,26 +52,54 @@ st.sidebar.header("Data das informações: \n" + str(date_select[0]) + " até \n
 
 df = load_data(date_select)
 
+def classifyOpMode(valor):
+    if valor > 0.2:
+        return 'On'
+    elif valor < 0.05:
+        return 'Off'
+    else:
+        return 'StandBy'
+
+currentAvgOld = df['current'].iloc[:-4000].mean()
+powerFacAvgOld = df['power_factor_calc'].iloc[:-4000].mean()
+opModeOld = df['current'].iloc[:-4000].apply(classifyOpMode)
+contOpModeOld = opModeOld.value_counts()
+qtyOnOld = contOpModeOld['On']
+qtyStandByOld = contOpModeOld['StandBy']
+qtyOffOld = contOpModeOld['Off']
+
+
+
 with st.container():
     # st.write("Tensão")
     # st.line_chart(df.rename(columns = {'voltage': 'Tensão (V)'})['Tensão (V)'], use_container_width=True) #Tensão
-    fig = px.line(df, x = 'moment', y = 'voltage')
+    fig = px.line(df, x = 'moment', y = 'voltage', title='Tensão [V]', labels = {'voltage': 'Tensão', 'moment': 'Horário'})
     st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    st.write("Corrente")
-    st.line_chart(df.rename(columns = {'current': 'Corrente (A)'})['Corrente (A)'], use_container_width=True) #Corrente 
+    # st.write("Corrente")
+    # st.line_chart(df.rename(columns = {'current': 'Corrente (A)'})['Corrente (A)'], use_container_width=True) #Corrente 
+    fig = px.line(df, x = 'moment', y = 'current', title='Corrente [A]', labels = {'current': 'Corrente', 'moment': 'Horário'})
+    st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    st.write("Potência")
-    st.line_chart(df.rename(columns = {'power_W': 'Potência (W)'})['Potência (W)'], use_container_width=True) #Potencia
+    # st.write("Potência")
+    # st.line_chart(df.rename(columns = {'power_W': 'Potência (W)'})['Potência (W)'], use_container_width=True) #Potencia
+    fig = px.line(df, x = 'moment', y = 'power_W', title='Potência [W]', labels = {'power_W': 'Potência', 'moment': 'Horário'})
+    st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    st.write("Energia")
-    st.line_chart(df.rename(columns = {'energy_WH': 'Energia (W/h)'})['Energia (W/h)'], use_container_width=True) #Energia
+    # st.write("Energia")
+    # st.line_chart(df.rename(columns = {'energy_WH': 'Energia (W/h)'})['Energia (W/h)'], use_container_width=True) #Energia
+    fig = px.line(df, x = 'moment', y = 'energy_WH', title='Energia [W/h]', labels = {'energy_WH': 'Energia', 'moment': 'Horário'})
+    st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    st.write("Fator de Potência")
-    st.line_chart(df.rename(columns = {'power_factor_measured': 'Fat de Pot Medido', 'power_factor_calc': 'Fat de Pot Calc'})[['Fat de Pot Medido', 'Fat de Pot Calc']], use_container_width=True) #Fator de Potência
+    # st.write("Fator de Potência")
+    # st.line_chart(df.rename(columns = {'power_factor_measured': 'Fat de Pot Medido', 'power_factor_calc': 'Fat de Pot Calc'})[['Fat de Pot Medido', 'Fat de Pot Calc']], use_container_width=True) #Fator de Potência
+    fig = px.line(df, x = 'moment', y = ['power_factor_measured', 'power_factor_calc'], title='Fator de Potência', color_discrete_map = {'power_factor_measured': 'green', 'power_factor_calc': 'blue'} , labels = {'power_factor_measured': 'Medido', 'power_factor_calc': 'Calculado', 'moment': 'Horário'})
+    st.plotly_chart(fig, use_container_width=True)
 with st.container():
-    st.write("Ângulo de Fase")
-    st.line_chart(df.rename(columns = {'phase_angle_measured': 'Âng de Fase Medido (Graus)', 'phase_angle_calc': 'Âng de Fase Calc (Graus)'})[['Âng de Fase Medido (Graus)', 'Âng de Fase Calc (Graus)']], use_container_width=True) #Ângulo de Fase
+    # st.write("Ângulo de Fase")
+    # st.line_chart(df.rename(columns = {'phase_angle_measured': 'Âng de Fase Medido (Graus)', 'phase_angle_calc': 'Âng de Fase Calc (Graus)'})[['Âng de Fase Medido (Graus)', 'Âng de Fase Calc (Graus)']], use_container_width=True) #Ângulo de Fase
+    fig = px.line(df, x = 'moment', y = ['phase_angle_measured', 'phase_angle_calc'], title='Ângulo de Fase [Graus]', color_discrete_map = {'phase_angle_measured': 'green', 'phase_angle_calc': 'blue'} , labels = {'phase_angle_measured': 'Medido', 'phase_angle_calc': 'Calculado', 'moment': 'Horário'})
+    st.plotly_chart(fig, use_container_width=True)
 with st.container():
     st.write("Modos de operação")
     chart_data = pd.DataFrame(np.random.randn(20,3), columns=["a", "b", "c"]) #Chart com a qtd de vezes do modo de operação
@@ -80,9 +108,17 @@ with st.container():
 
 #Para as métricas de modo de operação, quantas vezes ficou ligado, quantas vezes desligado, o outro modo de operação lá e por fim a qtd de Ah do período
 with st.sidebar:
-    st.metric("Humidity", "86%", "4%")
-    st.metric("Wind", "9 mph", "-8%")
-    st.metric("Temperature", "70 °F", "1.2 °F")
-    st.metric("Corrente (Ah)", "1000 Ah", "2 Ah")
+    currentAvg = df['current'].mean()
+    powerFacAvg = df['power_factor_calc'].mean()
+    opMode = df['current'].apply(classifyOpMode)
+    contOpMode = opMode.value_counts()
+    qtyOn = contOpMode['On']
+    qtyStandBy = contOpMode['StandBy']
+    qtyOff = contOpMode['Off']
+    st.metric("Corrente Média no período",  str(round(currentAvg, 3)) + " A", str(round(currentAvgOld-currentAvg, 3)) + " A" )
+    st.metric("Fator de Potência calculado no período", round(powerFacAvg, 3), round(powerFacAvgOld-powerFacAvg, 3))
+    st.metric("Quantidade de vezes ligado durante o período", str(round(qtyOn, 3)) + ' vezes', str(round(qtyOnOld-qtyOn, 3)) + ' vezes')
+    st.metric("Quantidade de vezes em standby durante o período", str(round(qtyStandBy, 3)) + ' vezes', str(round(qtyStandByOld-qtyStandBy, 3)) + ' vezes')
+    st.metric("Quantidade de vezes desligado durante o período", str(round(qtyOff, 3)) + ' vezes', str(round(qtyOffOld-qtyOff, 3)) + ' vezes')
 
 st.text('Lucas Moletta')
